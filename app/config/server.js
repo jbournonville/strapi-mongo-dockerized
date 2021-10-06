006178c5bf3530e54cf88046743be73480af192c
Julien BOURNONVILLE <julien.bournonville@routhiau.fr>
2021-10-06 17:33:52 +0200

    strapi running

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd60ee3f3199cceac9d42eca265281ece'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '90bd904f-bf6f-43e3-9c3e-b42a28508fb4'
+};
